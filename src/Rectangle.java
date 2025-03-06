8d016d6fd17762ac0a67a52cdfa635108589f1bd
ASUS <successolga118@gmail.com>
2025-03-06 22:17:59 +0300

    commit3

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,28 @@
 public class Rectangle {
+    double sideA;
+    double sideB;
+
+    public Rectangle(double sideA, double sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "sideA=" + sideA +
+                ", sideB=" + sideB +
+                '}';
+    }
+    public double calcArea(){
+        return sideA*sideB;
+    }
 }
