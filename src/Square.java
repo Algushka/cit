3a8af104fd7ae06aa16fcc72527835838c3bb9eb
ASUS <successolga118@gmail.com>
2025-03-06 23:33:48 +0300

    equals

--- src/Square.java
+++ src/Square.java
@@ -2,7 +2,7 @@ public class Square extends Rectangle {
     public Square(double side) {
         super(side, side);
     }
-
+//hkjh
     @Override
     public String toString() {
         return String.format("Квадрат %.2f, площадь: %.2f",getSideA(),calcArea());
